d4a9917697b2dd96bb077d831632aca70281a9b9
Benjamin Herro <benjamincsherro@hotmail.com>
2018-01-24 16:46:29 +1100

    Updated everything
    -Removed colour transition effect
    -Added final html string builder

--- js/script.js
+++ js/script.js
@@ -4,113 +4,66 @@
 // $.getJSON('quotes_json.json', function(json) {
 // 	console.log(json);
 // });
-var quoteClass = document.querySelector('.quote');
-var sourceClass = document.querySelector('.source');
-var citationClass = document.querySelector('span.citation');
-var yearClass = document.querySelector('span.year');
 var totalDiv = document.getElementById('quote-box');
 var colourRef = document.body.style;
 var buttonRef = document.getElementById('loadQuote').style;
-var initR = 137;
-var initG = 204;
-var initB = 114;
+// var initR = 137;
+// var initG = 204;
+// var initB = 114;
 var intervalID = window.setInterval(function() {
 	printQuote();
 	fadeIn(totalDiv);
-	getRandomColour();
-}, 12000)
+}, 12000);
+var intervalID2 = window.setInterval(getRandomColour, 12000);
 
 function getRandomQuote(myObj) {
 	var randIndex = Math.floor(Math.random() * myObj.length);
 	return myObj[randIndex];
 };
 
-// function getRandomColour() {
-// 	var r = Math.floor(Math.random() * 256);
-// 	var g = Math.floor(Math.random() * 256);
-// 	var b = Math.floor(Math.random() * 256);
-
-// 	var timer = setInterval(function() {
-// 		if (rDiff === 0 && gDiff === 0 && bDiff === 0) {
-// 				clearInterval(timer);
-// 		} else {
-// 				if (r > initR) {
-// 				var rDiff = r - initR;
-// 				initR += 1;
-// 			} else if (r < initR) {
-// 				var rDiff = initR - r;
-// 				initR -= 1;
-// 			} else if (r === initR) {
-// 				var rDiff = 0;
-// 			};
-// 			if (g > initG) {
-// 				var gDiff = g - initG;
-// 				initG += 1;
-// 			} else if (g < initG) {
-// 				var gDiff = initG - g;
-// 				initG -= 1;
-// 			} else if (g === initG) {
-// 				var gDiff = 0;
-// 			};
-// 			if (b > initB) {
-// 				var bDiff = b - initB;
-// 				initB += 1;
-// 			} else if (b < initB) {
-// 				var bDiff = initB - b;
-// 				initB -= 1;
-// 			} else if (b === initB) {
-// 				var bDiff = 0;
-// 			};
-// 		};
-// 		colourRef.backgroundColor = 'rgb('+ initR + ',' + initG + ',' + initB + ')';
-// 		// buttonRef.backgroundColor = 'rgb('+ initR + ',' + initG + ',' + initB + ')';
-// 	}, 10);
-// 	// colourRef.backgroundColor = 'rgb('+ r + ',' + g + ',' + b + ')';
-// };
-
 function getRandomColour() {
 	var r = Math.floor(Math.random() * 256);
 	var g = Math.floor(Math.random() * 256);
 	var b = Math.floor(Math.random() * 256);
-	var rDiff = Math.abs(r - initR);
-	var gDiff = Math.abs(g - initG);
-	var bDiff = Math.abs(b - initB);
-	var timer = setInterval(function() {
-		if (rDiff === 0 && gDiff === 0 && bDiff === 0) {
-				clearInterval(timer);
-		} else {
-				if (r > initR) {
-				rDiff -= 1;
-				initR += 1;
-			} else if (r < initR) {
-				rDiff -= 1;
-				initR -= 1;
-			} else if (r === initR) {
-				rDiff = 0;
-			};
-			if (g > initG) {
-				gDiff -= 1;
-				initG += 1;
-			} else if (g < initG) {
-				gDiff -= 1;
-				initG -= 1;
-			} else if (g === initG) {
-				gDiff = 0;
-			};
-			if (b > initB) {
-				bDiff -= 1;
-				initB += 1;
-			} else if (b < initB) {
-				bDiff -= 1;
-				initB -= 1;
-			} else if (b === initB) {
-				bDiff = 0;
-			};
-		};
-		colourRef.backgroundColor = 'rgb('+ initR + ',' + initG + ',' + initB + ')';
-		// buttonRef.backgroundColor = 'rgb('+ initR + ',' + initG + ',' + initB + ')';
-	}, 10);
-	// colourRef.backgroundColor = 'rgb('+ r + ',' + g + ',' + b + ')';
+	// var rDiff = Math.abs(r - initR);
+	// var gDiff = Math.abs(g - initG);
+	// var bDiff = Math.abs(b - initB);
+	// var timer = setInterval(function() {
+	// 	if (rDiff === 0 && gDiff === 0 && bDiff === 0) {
+	// 			clearInterval(timer);
+	// 	} else {
+	// 			if (r > initR) {
+	// 			rDiff -= 1;
+	// 			initR += 1;
+	// 		} else if (r < initR) {
+	// 			rDiff -= 1;
+	// 			initR -= 1;
+	// 		} else if (r === initR) {
+	// 			rDiff = 0;
+	// 		};
+	// 		if (g > initG) {
+	// 			gDiff -= 1;
+	// 			initG += 1;
+	// 		} else if (g < initG) {
+	// 			gDiff -= 1;
+	// 			initG -= 1;
+	// 		} else if (g === initG) {
+	// 			gDiff = 0;
+	// 		};
+	// 		if (b > initB) {
+	// 			bDiff -= 1;
+	// 			initB += 1;
+	// 		} else if (b < initB) {
+	// 			bDiff -= 1;
+	// 			initB -= 1;
+	// 		} else if (b === initB) {
+	// 			bDiff = 0;
+	// 		};
+	// 	};
+	// 	// colourRef.backgroundColor = 'rgb('+ initR + ',' + initG + ',' + initB + ')';
+	// 	// buttonRef.backgroundColor = 'rgb('+ initR + ',' + initG + ',' + initB + ')';
+	// }, 10);
+	colourRef.backgroundColor = 'rgb('+ r + ',' + g + ',' + b + ')';
 };
 
 function fadeIn(target) {
@@ -128,18 +81,24 @@ function fadeIn(target) {
 
 function printQuote() {
 	var calledQuote = getRandomQuote(quotes);
-	quoteClass.innerHTML = calledQuote.quote;
-	sourceClass.innerHTML = calledQuote.source;
+	var rawHTML = '';
+	var spanHTML = '';
+
 	if (calledQuote.citation !== "None") {
-		citationClass.innerHTML = calledQuote.citation;
+		spanHTML += '<span class="citation">' + calledQuote.citation + '</span>';
 		};
 	if (calledQuote.year !== "None") {
-		yearClass.innerHTML = calledQuote.year;
-		};
-	}
+		spanHTML += '<span class="year">' + calledQuote.year + '</span>';
+	};
+
+	rawHTML += '<p class="quote">' + calledQuote.quote + '</p>';
+	rawHTML += '<p class="source">' + calledQuote.source + spanHTML + '</p>';
+
+	totalDiv.innerHTML = rawHTML;
+	};
 
 document.getElementById('loadQuote').addEventListener("click", function() {
 	printQuote();
-	fadeIn(totalDiv);
 	getRandomColour();
+	fadeIn(totalDiv);
 }, false);
